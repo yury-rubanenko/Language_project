6fb15641eaf6dddeba6949a4c2a5417cbe88ca64
terror53421 <urarubanenko@gmail.com>
2023-08-07 14:11:08 +0300

    serialaizer del words

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,22 +1,9 @@
 from rest_framework import serializers
-from platforms.models import Word, UserWord
 from .models import User
 
-
-class WordSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Word
-        fields = '__all__'
-
-class UserWordSerializer(serializers.ModelSerializer):
-    word = WordSerializer()
-
-    class Meta:
-        model = UserWord
-        fields = ('word', 'learned_at')
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'words')
+        fields = ('id', 'username', 'email')
     
