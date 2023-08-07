7940888e5ba24416233cbcdef794e4db1f74768f
terror53421 <urarubanenko@gmail.com>
2023-08-07 13:33:27 +0300

    viewsets

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
 
+
+router = DefaultRouter()
+router.register(r'user_profile', views.UserProfileViewSet, basename='user-profile')
+
 urlpatterns = [
-    path('user_profile/', views.UserProfileView.as_view(), name='user_profile'),
+    path('', include(router.urls)),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,12 +1,13 @@
-from rest_framework.generics import ListAPIView
+from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from .models import User
 from .serializers import UserSerializer
 
-class UserProfileView(ListAPIView):
+class UserProfileViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]
-
+    
     def get_queryset(self):
         user = self.request.user
         return User.objects.filter(pk=user.pk)
+    
