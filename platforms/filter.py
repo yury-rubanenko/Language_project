b51b3617a4f21c7d2e5bbd70325c18ef58af02b5
terror53421 <urarubanenko@gmail.com>
2023-08-12 13:38:32 +0300

    app

---
+++ platforms/filter.py
@@ -0,0 +1,26 @@
+import django_filters
+from django.forms.widgets import DateInput
+from .models import UserWord
+
+
+class ReplacementFilter(django_filters.FilterSet):
+    learned_at = django_filters.DateFilter(
+        field_name='learned_at',
+        lookup_expr='gte',
+        widget=DateInput(attrs={'type': 'date'})
+    )
+    learned_at__lte = django_filters.DateFilter(
+        field_name='learned_at',
+        lookup_expr='lte',
+        widget=DateInput(attrs={'type': 'date'})
+    )
+    not_learned = django_filters.BooleanFilter(
+        field_name='learned_at',
+        lookup_expr='isnull',
+        exclude=True
+    )
+
+    class Meta:
+        model = UserWord
+        fields = []
+
--- platforms/views.py
+++ platforms/views.py
@@ -3,28 +3,19 @@ from django_filters.rest_framework import DjangoFilterBackend
 from .models import UserWord, Word
 from .serializers import UserWordSerializer, WordSerializer
 from rest_framework import permissions
-from rest_framework.filters import SearchFilter, OrderingFilter
+from .filter import ReplacementFilter
 
 
 class UserWordsListView(viewsets.ReadOnlyModelViewSet):
     serializer_class = UserWordSerializer
     permission_classes = [permissions.IsAuthenticated]
-    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
-    filterset_fields = ['learned_at']
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = ReplacementFilter
     search_fields = ['word__word', 'word__translation']
     ordering_fields = ['learned_at']
-
-    def get_queryset(self):
-        user = self.request.user
-        queryset = UserWord.objects.filter(user=user)
-
-        status = self.request.query_params.get('status', None)
-        if status == 'learned':
-            queryset = queryset.exclude(learned_at=None)
-        elif status == 'unlearned':
-            queryset = queryset.filter(learned_at=None)
-        return queryset
     
+    queryset = UserWord.objects.all() 
+
 
 class WordListView(viewsets.ReadOnlyModelViewSet):
     serializer_class = WordSerializer
