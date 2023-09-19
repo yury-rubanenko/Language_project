8d272da04b67efd83ce3c7fa9380ae52227e658b
terror53421 <urarubanenko@gmail.com>
2023-09-19 15:39:08 +0300

    login loagout signup passchange urls

--- Language_Project/urls.py
+++ Language_Project/urls.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
 from django.urls import include, path, re_path
-
+from allauth.account.views import (
+    LoginView, 
+    LogoutView, 
+    SignupView,
+    PasswordChangeView,
+)
 from .swagger_settings import schema_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("accounts/", include("allauth.urls")),
+    path("accounts/login/", LoginView.as_view(), name="account_login"),
+    path("accounts/logout/", LogoutView.as_view(), name="account_logout"),
+    path("accounts/signup/", SignupView.as_view(), name="account_signup"),
+    path("accounts/password/change/", PasswordChangeView.as_view(), name="account_change_password"),
     path("api/v1/users/", include("profiles.urls")),
     path("api/v1/platforms/", include("platforms.urls")),
     path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
