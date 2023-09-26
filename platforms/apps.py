2929fca3f2a01a42aaea64cecac59a356d1795c6
terror53421 <urarubanenko@gmail.com>
2023-09-26 16:36:33 +0300

    fix flake8 for import

--- platforms/apps.py
+++ platforms/apps.py
@@ -1,9 +1,10 @@
 from django.apps import AppConfig
 
 
+# flake8: noqa
 class PlatformsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "platforms"
 
     def ready(self):
-        pass
+        import platforms.signals
--- platforms/signals.py
+++ platforms/signals.py
@@ -12,13 +12,14 @@ def assign_default_tag(sender, instance, created, **kwargs):
 
 
 @receiver(m2m_changed, sender=Word.tags.through)
-def update_word_tags(sender, instance, action, **kwargs):
+def update_word_tags(sender, instance: Word, action: str, **kwargs):
     if action == "post_remove":
         if not instance.tags.exists():
             default_tag, created = Tag.objects.get_or_create(name="tag_not_set")
             instance.tags.add(default_tag)
 
-    if action == "post_add":
-        if not instance.tags.exists():
-            default_tag, created = Tag.objects.get_or_create(name="tag_not_set")
-            instance.tags.add(default_tag)
+    elif action == "post_add":
+        instance.tags.through.filter(word_id=instance.id, tag__name="tag_not_set").delete()
+        # default_tag, created = Tag.objects.get_or_create(name="tag_not_set")
+        # if default_tag in instance.tags.all():
+        #     instance.tags.remove(default_tag)
